ef5b0a84b8c9fd94e4804ca815e004d4de8a79ef
Douglas Barcelos <doug-pr@hotmail.com>
2024-10-03 17:50:15 -0300

    Adicionando Rodapé

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+st.set_page_config(
+    page_title="Minha Primeira Página de Machine Learning",
+    page_icon="👋",
+)
+
 df = pd.read_csv("Pizzas.csv")
 
 modelo = LinearRegression()
@@ -19,3 +24,5 @@ if diametro:
     preco_previsto = modelo.predict([[diametro]])[0][0]
     st.write(f"O Valor da pizza com diâmetro de {diametro:.2f} cm é de R$ {preco_previsto:.2f}!")
     st.balloons()
+
+<b>Desenvolvido por Douglas Barcelos</b>
